162eaf2e0e5c29a147bbe1d725d8d1a787c806be
Israel Block <israel.block@opet.edu.br>
2019-03-25 21:08:56 -0300

    Initial commit

---
+++ app/src/main/java/uniopet/edu/br/filmes/Telalogado.java
@@ -0,0 +1,13 @@
+package uniopet.edu.br.filmes;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Telalogado extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_telalogado);
+    }
+}
